046b27b4dcbe8144ba16221767cebe9b24e30351
sthasaroj13 <sarojshrestha651@gmail.com>
2024-04-10 13:09:39 +0545

    height change in skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -12,9 +12,9 @@ import sql from '../Image/mysql.png'
 
 function Skills() {
   return (
-    <div className="max-w-screen-xl max-h-64 mx-auto px-4 sm:px-6 lg:px-8 mt-10 mb-[12rem]">
+    <div className="max-w-screen-xl max-h-[23.5rem] mx-auto px-4 sm:px-6 lg:px-8 mt-1 mb-[10rem]">
       <h1 className="txt-heading text-cyan-400 ml-4 sm:ml-24 font-bold underline">Skills</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-5">
         <img src={html} alt="HTML" className="w-16 md:w-24 lg:w-32 h-auto" />
         <img src={css} alt="CSS" className=" w-24 md:w-44" />
         <img src={js} alt="JavaScript" className="w-16 md:w-24 lg:w-32 h-auto" />
@@ -23,7 +23,7 @@ function Skills() {
         <img src={express} alt="express" className="w-16 md:w-24 lg:w-32 h-auto" />
         <img src={mongo} alt="mongo" className="w-16 md:w-24 lg:w-32 h-auto" />
         <img src={figma} alt="figma" className="w-16 md:w-24 lg:w-32 h-auto" />
-        <img src={php} alt="php" className="w-24 md:w-44 lg:w-32 h-auto" />
+        <img src={php} alt="php" className="w-24 md:w-28 lg:w-32 h-auto" />
         <img src={sql} alt="mysql" className="w-16 md:w-24 lg:w-32 h-auto" />
       </div>
     </div>
